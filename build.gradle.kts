6720561359286578d01682e7e76140cd8511e1bd
Fernando <fernando.csconceicao@outlook.com>
2024-08-26 12:48:35 -0300

    p

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.3"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "br.com.camarada"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	implementation("org.springframework.boot:spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
